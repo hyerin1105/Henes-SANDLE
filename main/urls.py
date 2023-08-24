ddc95ad4152d3165f20967672bd2edec7d34a0b0
hyerin1105 <hyerin328115@likelion.org>
2023-08-24 18:55:43 +0900

    SANDLE

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+import main.views
+
+urlpatterns = [
+    path('', main.views.main, name="main"),
+    path('goods', main.views.goods, name="goods"),
+]
---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+
+# Create your views here.
+def main(request):
+    return render(request,'main.html')
+def goods(request):
+    return render(request,'goods.html')
