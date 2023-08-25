f8feaf7c21aada779347fec45be9874e263f20a7
hyerin1105 <hyerin328115@likelion.org>
2023-08-25 10:53:16 +0900

    SANDLE

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ import main.views
 urlpatterns = [
     path('', main.views.main, name="main"),
     path('goods', main.views.goods, name="goods"),
+    path('complete', main.views.complete, name="complete"),
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,5 @@ def main(request):
     return render(request,'main.html')
 def goods(request):
     return render(request,'goods.html')
+def complete(request):
+    return render(request,'complete.html')
