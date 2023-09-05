bb890cba51f7ec6c0c77315c2d6aee683742eff0
hyerin1105 <hyerin328115@likelion.org>
2023-09-05 14:51:55 +0900

    pardon?

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ClForm
+from .forms import LoginForm
 from .models import Customer
 from django.http import Http404
 from django.core.exceptions import PermissionDenied
@@ -15,6 +15,7 @@ def loading(request):
 def checking(request):
     return render(request, 'checking.html')
 
+@login_required(login_url='/login/')
 def create(request):
     if request.method == 'POST':
         post = Customer()
@@ -28,8 +29,10 @@ def create(request):
 def main(request):
     return render(request,'main.html')
 
+@login_required(login_url='/login/')
 def goods(request, customer_id): #views.py의 pk변수명과 urls.py의 변수명은 같아야 함
     customer = get_object_or_404(Customer, pk=customer_id) #모델과 pk를 customer_id라고 부를거야
+    customer_form = LoginForm()
     return render(request,'goods.html', {'customer' : customer}) #값을 보낼거임
 
 def complete(request):
