47b97288c8de853975738820ec479697d379e0e7
hyerin1105 <hyerin328115@likelion.org>
2023-09-06 17:59:42 +0900

    sandle'

--- management/views.py
+++ management/views.py
@@ -7,50 +7,24 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 def join(request):
     if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
-            return redirect('goods')
-    else:
-        form = UserForm()
-    return render(request, 'management/join.html', {'form': form})
+        new_user = User.objects.create_user(address=request.POST['address'], password=request.POST['password'])
+        auth.login(request, new_user)
+        return redirect('goods')
+        
+    return render(request, 'join.html')
 
 def login(request):
     if request.method == "POST":
-        password=request.POST.get('password', None)
-        user=auth.authenticate(request, password=password)
+        address=request.POST.get('address',None)
+        password=request.POST.get('password',None)
+        user=auth.authenticate(request, address=address, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('main')
+            return redirect('goods')
         else:
             return HttpResponse('Login failed. Try again.')
     else:
-        return render(request, 'login.html')
-"""
-
-def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        password = request.POST.get('password', None)
-
-    res_data = ()
-
-    if not(password):
-        res_data['error'] = "비밀번호를 다시 입력해주세요."
-    else:
-        if check_password(password, customer.password):
-            request.session['customer'] = customer.id
-            return redirect('/')
-        else:
-            res_data['error'] = "비밀번호가 틀렸습니다."
-
-    return render(request, 'login.html', res_data)
-"""
+        return render(request, 'management/login.html')
 
 def logout(request):
     if request.session.get('customer'):
