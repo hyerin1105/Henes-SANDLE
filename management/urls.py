ad25df7604766d4ebec5e05dee0ea1c53330cb02
hyerin1105 <hyerin328115@likelion.org>
2023-09-06 17:13:58 +0900

    error

--- management/urls.py
+++ management/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import join
 from management import views
 
 """
