d3b12dd12196df1593ca9115bdf82421fe1672ea
hyerin1105 <hyerin328115@likelion.org>
2023-09-06 17:11:12 +0900

    sandle

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import login, join
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import join
 from management import views
-from main import views
 
 """
 urlpatterns = [
@@ -13,11 +12,10 @@ urlpatterns = [
 ]
 """
 
-app_name = 'management', 'main'
+app_name = 'management'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='management/login.html'), name='login'),
-    ### 로그아웃 추가 ####
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), # 로그아웃 추가
     path('join/', views.join, name='join'),
 ]
