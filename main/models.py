3c32e18820de7390df3142a48fee94ca78a6d9a1
hyerin1105 <hyerin328115@likelion.org>
2023-09-15 14:24:42 +0900

    sandle

--- main/models.py
+++ main/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
 # Create your models here.
 class Customer(models.Model):
@@ -9,49 +8,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.address
-
-
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, password=None):
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, address, password):
-        user = self.create_user(
-            address,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):
-    address = models.AddressField(
-        verbose_name='address',
-        max_length=15,
-        unique=True,
-    )
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'address'
-
-    def __str__(self):
-        return self.address
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
--- management/views.py
+++ management/views.py
@@ -4,41 +4,52 @@ from main.models import Customer
 from django.contrib.auth import get_user_model  # get_user_model 변수로 데이터베이스 조회 가능
 from django.http import HttpResponse  # 화면에 텍스트를 띄워주기 위함
 from django.contrib import auth  # 유저 정보 검증하기 위함
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required  # 데코레이터 함수 생성
 
 
-def sign_up_view(request):  # 회원가입 함수
-    if request.method == 'GET':
-        return render(request, 'join.html')
-    elif request.method == 'POST':  # 회원가입 정보 입력 후 가입하기 클릭시
-        address = request.POST.get('address', None)  # 이름
-        password = request.POST.get('password', None)  # 비밀번호
-        password2 = request.POST.get('password2', None)  # 비밀번호 확인
-
-        if password != password2:  # 비밀번호와 비밀번호 확인이 같지 않으면
-            return render(request, 'join.html')  # 페이지 다시 로드
-        
-        else:  # 같으면
-            Customer.objects.create_user(address=address, password=password)  # 데이터베이스에 이름, 비밀번호, 자기소개를 저장
-            return redirect('checking.html')  # 고객용 메인 페이지로 이동
-            
-def sign_in_view(request):
+def signup(request):  # 회원가입 함수
+    # signup 으로 POST 요청이 왔을 때, 새로운 유저를 만드는 절차를 밟는다.
     if request.method == 'POST':
-        address = request.POST.get('address', None)
-        password = request.POST.get('password', None)
+        # password와 confirm에 입력된 값이 같다면
+        if request.POST['password'] == request.POST['confirm']:
+            # user 객체를 새로 생성
+            user = Customer.objects.create_user(address=request.POST['address'], password=request.POST['password'])
+            # 로그인 한다
+            auth.login(request, user)
+            return redirect('/')
+    # signup으로 GET 요청이 왔을 때, 회원가입 화면을 띄워준다.
+    return render(request, 'signup.html')
 
-        me = auth.authenticate(request, address=address, password=password)
-        # auth.authenticate() 로 암호화된 데이터베이스를 추가적인 코드없이 조회 및 검증이 가능
+            
+# 로그인
+@csrf_exempt
+def login(request):
+    # login으로 POST 요청이 들어왔을 때, 로그인 절차를 밟는다.
+    if request.method == 'POST':
+        # login.html에서 넘어온 username과 password를 각 변수에 저장한다.
+        address = request.POST['address']
+        password = request.POST['password']
+
+        # 해당 username과 password와 일치하는 user 객체를 가져온다.
+        customer = auth.authenticate(request, address=address, password=password)
         
-        if me is not None:  # 입력한 정보가 존재하고 일치한다면
-            auth.login(request, me)  # auth.login() 으로 토큰을 생성 및 전달
-            return render(request, 'goods.html', {'customer_id': customer_id})
-        else:  # 입력한 정보가 없거나 틀리다면
-            return redirect('management/login')  # 페이지 다시 로드
-    elif request.method == 'GET':
-        return render(request, 'management/join.html')
-    
-@login_required  # 로그인 인증되어있을때만 실행됨
+        # 해당 user 객체가 존재한다면
+        if customer is not None:
+            # 로그인한다
+            auth.login(request, customer)
+            return redirect('goods')
+        # 존재하지 않는다면
+        else:
+            # 딕셔너리에 에러메세지를 전달하고 다시 login.html 화면으로 돌아간다.
+            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
+    # login으로 GET 요청이 들어왔을때, 로그인 화면을 띄워준다.
+    else:
+        return render(request, 'login.html')
+
+# 로그아웃
+@login_required  # 로그인 인증되어있을 때만 실행됨
 def logout(request):
-    auth.logout(request)  # 장고 내장 기능인 로그아웃
+    # logout으로 POST 요청이 들어왔을 때, 로그아웃 절차를 밟는다.
+    auth.logout(request)
     return redirect('/')
